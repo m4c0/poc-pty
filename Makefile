c3cfa205b7e77ab287e41333f31490817bf1b57e
Eduardo Costa <m4c0@users.noreply.github.com>
2023-08-30 12:10:58 +0100

    kinda worky with forkpty
    still some issues quitting VIM... but who is crazy enought to quit some
    awesome editor?

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+all: test.exe
+	./$<
+
 test.exe: test.cpp
 	clang++ -std=c++20 -o $@ $<
 
+.PHONY: all
--- test.cpp
+++ test.cpp
@@ -1,9 +1,14 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <util.h>
 
-void uga(int fd, const char *msg) { write(fd, msg, strlen(msg)); }
+void uga(int fd, const char *msg) {
+  puts(msg);
+  write(fd, msg, strlen(msg));
+}
 
 int main() {
   char *args[2];
@@ -13,36 +18,45 @@ int main() {
   char *env[1];
   env[0] = 0;
 
-  int err = open("/tmp/err", O_WRONLY | O_CREAT | O_TRUNC);
-  if (!err) {
-    perror("nope");
-    return 1;
-  }
-
-  int out = open("/tmp/out", O_WRONLY | O_CREAT | O_TRUNC);
+  int out = open("test.out", O_WRONLY | O_CREAT | O_TRUNC);
   if (!out) {
     perror("nope");
     return 1;
   }
 
-  int fd[2];
-  pipe(fd);
+  winsize sz{
+      .ws_row = 120,
+      .ws_col = 60,
+  };
 
-  close(STDIN_FILENO);
-  close(STDOUT_FILENO);
-  close(STDERR_FILENO);
-
-  dup2(fd[0], STDIN_FILENO);
-  dup2(out, STDOUT_FILENO);
-  dup2(err, STDERR_FILENO);
-
-  uga(fd[1], ":echo &columns\n");
-  uga(fd[1], ":echo &lines\n");
-  uga(fd[1], ":echo &ttytype\n");
-  uga(fd[1], ":echo &term\n");
-  uga(fd[1], ":echo has_key(environ(), 'HOME')\n");
-  uga(fd[1], ":set! all\n");
-  uga(fd[1], ":q\n");
-
-  execve("/usr/local/bin/vim", args, env);
+  int pri;
+  switch (forkpty(&pri, nullptr, nullptr, &sz)) {
+  case -1:
+    return 1;
+  case 0:
+    dup2(out, STDOUT_FILENO);
+    execve("/usr/local/bin/vim", args, env);
+    break;
+  default:
+    uga(pri, ":echo &columns\n");
+    uga(pri, ":echo &lines\n");
+    uga(pri, ":echo &ttytype\n");
+    uga(pri, ":echo &term\n");
+    uga(pri, ":echo has_key(environ(), 'HOME')\n");
+    uga(pri, ":set! all\n");
+    sleep(5);
+    uga(pri, "G");
+    sleep(1);
+    uga(pri, "\n");
+    sleep(1);
+    uga(pri, ":q\n");
+    sleep(1);
+    uga(pri, ":q\n");
+    sleep(1);
+    uga(pri, ":q\n");
+    sleep(1);
+    uga(pri, ":q\n");
+    wait(0);
+    break;
+  }
 }
